8300b91a553cf5424f3fb5c43ed81008e8dc7e81
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-04 21:31:15 -0700

    add usage comment to create_seeds.sh

--- create_seeds.sh
+++ create_seeds.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# note: call from the curriculum you want to process
+# NOTE: call from the curriculum folder you want to process
 
 # step 1: create the seed files
 
-# first part creates (outputs through pipe) a list of all the files in
-# LIFETOMATH directorys, subdirectories, and those subdirectories' directories
+# first part creates (and outputs through pipe) a list of all the files in
+# curriculum's directory, subdirectories, and those subdirectories' directories
 
 # second part has perl print a list of all the files that don't have "theory"
 # in them and that gets piped as output into python's stdin
@@ -15,4 +15,4 @@ ls ./**/**/*.html | perl -nle 'print if /(?<!theory)[0-9]+.html/' | python ~/Doc
 
 # step 2: grab all the _seed files and concatenate them into a large seed file
 rm ./curriculum_seed.rb
-find . -name '*.html_seed' -exec cat {} >> curriculum_seed.rb \;
+find . -name '*.html_seed' -exec cat {} >> ./curriculum_seed.rb \;
