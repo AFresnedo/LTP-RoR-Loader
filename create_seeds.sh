e09b7a3347fc42ca50894aa3aef11ddb215c9c45
AFresnedo <AFresnedo@users.noreply.github.com>
2017-08-04 21:08:07 -0700

    tidy filenames in seed file

--- create_seeds.sh
+++ create_seeds.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# step 1: create the seed files
+
 # first part creates (outputs through pipe) a list of all the files in
 # LIFETOMATH directorys, subdirectories, and those subdirectories' directories
 
@@ -8,3 +10,7 @@
 
 # third part: see python scripts for math-affirm-loader
 ls ../ma_files/LIFETOMATH/**/**/*.html | perl -nle 'print if /(?<!theory)[0-9]+.html/' | python ~/Documents/persProj/ma_loader/problem_input.py
+
+# step 2: grab all the _seed files and concatenate them into a large seed file
+rm ../ma_files/LIFETOMATH/curriculum_seed.rb
+find ../ma_files/LIFETOMATH -name '*.html_seed' -exec cat {} >> ../ma_files/LIFETOMATH/curriculum_seed.rb \;
