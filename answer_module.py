f766fcc621e51b82cfb011ba45e7407befe047b0
AFresnedo <AFresnedo@users.noreply.github.com>
2017-12-08 07:49:03 -0800

    code process default ans mod

--- answer_module.py
+++ answer_module.py
@@ -6,9 +6,8 @@ def processAnswers(i, o):
         # check for beginning of solution module and exit iff found
         if ':btype:' in line:
             break
-        # DETERMINE TYPE OF ANSWER
         # find :bans: line
-        if ':bans:' in line:
+        elif ':bans:' in line:
             # check for type, use default if missing
             if 'mc' in line:
                 processMultipleChoice(i, o)
@@ -19,9 +18,25 @@ def processAnswers(i, o):
 
 # process a single fill-in-the-boxes answer set
 def processDefault(i, o):
-    # process answer
+    # process answer values
+    o.write('#ANSWER TUPLE FOR PROBLEM P\n')
+    o.write('p.answer = Answer.new(values: "')
+    for line in i:
+        if ':eans:' in line:
+            break
+        o.write(line.strip())
+    assert ':eans:' in line
     # process interface
-    None
+    o.write('", interface: "')
+    for line in i:
+        if ':bansinf:' in line:
+            break
+        assert ':bansinf:' in line
+        for line in i:
+            if ':eansinf:' in line:
+                break
+            o.write(line.strip() + ' ')
+        o.write('")\n')
 
 # process a single standard multiple choice
 def processMultipleChoice(i, o):
@@ -34,30 +49,3 @@ def processCheckboxes(i, o):
     # process answer list
     # process question
     None
-
-    #  # Answer tuple
-    #  # write beginning of create command for p.Answer tuple
-    #  o.write('#ANSWER TUPLE FOR PROBLEM P' + "\n")
-    #  # get problem tuple from database (doesn't seem to work otherwise)
-    #  o.write('p.answer = Answer.new(values: "')
-    #  # write answer value(s)
-    #  for line in i:
-        #  # end of problem statement reached
-        #  if ':eans:' in line:
-            #  break
-        #  o.write(line.strip())
-    #  # end writing answer value(s)
-    #  assert ':eans:' in line
-    #  o.write('", interface: "')
-    #  # find interface text
-    #  for line in i:
-        #  if ':bansinf:' in line:
-            #  break
-    #  assert ':bansinf:' in line
-    #  # write interface
-    #  for line in i:
-        #  if ':eansinf:' in line:
-            #  break
-        #  if 'p>' not in line:
-            #  o.write(line.strip() + ' ')
-    #  o.write('")\n')
